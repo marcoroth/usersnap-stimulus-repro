307202f8105b7da3fb890b8bc2f080610f5f89fd
Marco Roth <marco.roth@intergga.ch>
2022-12-18 23:28:44 +0100

    WIP

--- main.js
+++ main.js
@@ -5,12 +5,13 @@ import { Application, Controller } from '@hotwired/stimulus'
 const application = Application.start()
 
 application.register('usersnap', class extends Controller {
-  initialize() {
-    console.log("initialize")
-    console.log("Usersnap in initialize", window.Usersnap)
-  }
 
   connect() {
-    console.log("connect")
+    console.log("data", this.data.get("controller"))
+  }
+
+  open() {
+    console.log("open", window.Usersnap)
+    window.Usersnap.logEvent('open');
   }
 })
